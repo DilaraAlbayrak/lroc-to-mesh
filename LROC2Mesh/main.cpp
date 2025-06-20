7a40da68735a675da10b4b3a469670df37219b00
DilaraAlbayrak <dilarasina.a@gmail.com>
2025-06-20 22:43:26 +0100

    visual studio .sln ve GDAL dependencies are built

---
+++ LROC2Mesh/main.cpp
@@ -0,0 +1,26 @@
+#include "gdal_priv.h"
+#include <iostream>
+#include <vector>
+
+int main() {
+    GDALAllRegister();
+    const char* filename = "NAC_DTM_APOLLO11_E480N0005_DEM1.IMG";
+
+    GDALDataset* dataset = (GDALDataset*)GDALOpen(filename, GA_ReadOnly);
+    if (!dataset) {
+        std::cerr << "Failed to open file.\n";
+        return 1;
+    }
+
+    GDALRasterBand* band = dataset->GetRasterBand(1);
+    int width = band->GetXSize();
+    int height = band->GetYSize();
+
+    std::vector<float> data(width * height);
+    band->RasterIO(GF_Read, 0, 0, width, height, data.data(), width, height, GDT_Float32, 0, 0);
+
+    std::cout << "Read elevation data: " << width << "x" << height << "\n";
+
+    GDALClose(dataset);
+    return 0;
+}
